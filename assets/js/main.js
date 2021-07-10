dce023f526c24e3204d71aed1c5c53827008261a
5612 <tienhv58@gmail.com>
2021-07-11 00:33:17 +0700

    complete modal and action

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
+
+// Searchbar
 const searchbarItems = $$(".header__searchbar-item");
 const searchbarItemActive = $(".header__searchbar-item.active");
 
@@ -60,24 +62,57 @@ categoryItems.forEach((item, index) => {
     };
 });
 
-
+// Modal
 // Get the modal
 const modal = document.getElementById("modal__container");
 
+const openModal = () => {
+    modal.style.display = "block";
+    $("body").style.overflow = "hidden";
+}
+
+const removeActive = () => {
+    $(".header__category-item.header__category--active").classList.remove("header__category--active");
+    $(".modal__body-item.modal__body--active").classList.remove("modal__body--active");
+}
+
 // When the user clicks the button, open the modal 
-$(".btn__globe-modal").onclick = function() {
-  modal.style.display = "block";
+$(".btn__globe-modal").onclick = function () {
+    openModal();
 }
 
 // When the user clicks on <span> (x), close the modal
-$(".modal--close").onclick = function() {
-  modal.style.display = "none";
+$(".modal--close").onclick = function () {
+    modal.style.display = "none";
+    $("body").style.overflow = "auto";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        $("body").style.overflow = "auto";
+    }
 }
 
+$$(".header__category-item").forEach((item, index) => {
+    item.onclick = function () {
+        removeActive();
+        this.classList.add("header__category--active");
+        $$(".modal__body-item")[index].classList.add("modal__body--active");
+    }
+})
+
+$$(".footernav_link")[0].onclick = () => {
+    openModal();
+    removeActive();
+    $$(".header__category-item")[0].classList.add("header__category--active");
+    $$(".modal__body-item")[0].classList.add("modal__body--active");
+}
+
+$$(".footernav_link")[1].onclick = () => {
+    openModal();
+    removeActive();
+    $$(".header__category-item")[1].classList.add("header__category--active");
+    $$(".modal__body-item")[1].classList.add("modal__body--active");
+}
